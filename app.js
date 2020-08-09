eed47699fe753171f474721c5dec15207b421ca9
Dailer Hurtado <dailer.hurtado0321@gmail.com>
2020-08-09 03:19:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.get("/error", function  (req, res) {
 app.get("/", async function  (req, res) {
   try {
     const user = await lib.UsersController.getCurrentUserData();
+    console.log("response", user.data)
     res.json(user.data);
     // res.send('<a href="/connect">Connect to Xero</a>');  
   } catch (error) {
