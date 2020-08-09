15e9cafeaf6c17e842cb899faf06c3d9d3005daa
Dailer Hurtado <dailer.hurtado0321@gmail.com>
2020-08-09 00:36:56 -0500

    update server

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 const lib = require('pipedrive');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
+const { response } = require("express");
 let app = express();
 
 app.set("port", process.env.PORT || 5000);
@@ -23,16 +24,57 @@ app.use(
 //   keys: ['key1']
 // }));
 lib.Configuration.apiToken = 'cc638af3ea2783059aae7e32b5b80e34c1f0d1f4';
-console.log(lib)
-app.get("/", async function  (req, res) {
+
+
+const oAuthManager = lib.OAuthManager;
+lib.Configuration.oAuthClientId = 'a0f664cbd953c9b2'; // OAuth 2 Client ID
+lib.Configuration.oAuthClientSecret = 'b4af2a4024bd590af337472559b1702dc8ea51c1'; // OAuth 2 Client Secret
+lib.Configuration.oAuthRedirectUri = 'http://localhost:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
+
+
+app.get('/', async (req, res) => {
+  if (req.session.token !== null && req.session.token !== undefined) {
+      // token is already set in the session
+      // now make API calls as required
+      // client will automatically refresh the token when it expires and call the token update callback
+      const user = await lib.UsersController.getCurrentUserData();
+
+      res.send(user);
+  } else {
+      const authUrl = oAuthManager.buildAuthorizationUrl();
+      alert(authUrl)
+      res.redirect(authUrl);
+  }
+});
+
+app.get('/callback', (req, res) => {
+  const authCode = req.query.code;
+  const promise = oAuthManager.authorize(authCode);
+
+  promise.then(() => {
+      req.session.token = lib.Configuration.oAuthToken;
+      res.redirect('/');
+  }, (exception) => {
+      res.redirect('/error')
+      // error occurred, exception will be of type lib/Exceptions/OAuthProviderException
+  });
+});
+app.get("/error", function  (req, res) {
   try {
-    const user = await lib.UsersController.getCurrentUserData();
-    res.json(user);
-    // res.send('<a href="/connect">Connect to Xero</a>');  
+    res.send('<a href="/">Try again</a>');  
   } catch (error) {
     res.json(error);
   }
 });
+// app.get("/", async function  (req, res) {
+//   try {
+//     const user = await lib.UsersController.getCurrentUserData();
+//     res.json(user);
+//     // res.send('<a href="/connect">Connect to Xero</a>');  
+//   } catch (error) {
+//     res.json(error);
+//   }
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
