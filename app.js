0b7aaced4b18a1062992d3e36d760d0b9e77fefb
Dailer Hurtado <dailer.hurtado0321@gmail.com>
2020-08-09 05:54:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
 const express = require("express");
 const session = require("express-session");
-const lib = require('pipedrive');
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
+const lib = require("pipedrive");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const { response } = require("express");
 let app = express();
 
@@ -23,15 +23,14 @@ app.use(
 //   name: 'session',
 //   keys: ['key1']
 // }));
-lib.Configuration.apiToken = 'cc638af3ea2783059aae7e32b5b80e34c1f0d1f4';
-
+lib.Configuration.apiToken = "cc638af3ea2783059aae7e32b5b80e34c1f0d1f4";
 
 const oAuthManager = lib.OAuthManager;
-lib.Configuration.oAuthClientId = 'de96c7b1775cbd03'; // OAuth 2 Client ID
-lib.Configuration.oAuthClientSecret = '96e47bc6458c799b36a4115aafdeabdb65075c81'; // OAuth 2 Client Secret
+lib.Configuration.oAuthClientId = "de96c7b1775cbd03"; // OAuth 2 Client ID
+lib.Configuration.oAuthClientSecret =
+  "96e47bc6458c799b36a4115aafdeabdb65075c81"; // OAuth 2 Client Secret
 // lib.Configuration.oAuthRedirectUri = 'http://104.248.2.159:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
-lib.Configuration.oAuthRedirectUri = 'http://localhost:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
-
+lib.Configuration.oAuthRedirectUri = "http://localhost:5000/callback"; // OAuth 2 Redirection endpoint or Callback Uri
 
 // app.get('/', async (req, res) => {
 //   if (req.session.token !== null && req.session.token !== undefined) {
@@ -48,40 +47,56 @@ lib.Configuration.oAuthRedirectUri = 'http://localhost:5000/callback'; // OAuth 
 //   }
 // });
 
-app.get('/callback', (req, res) => {
+app.get("/callback", (req, res) => {
   const authCode = req.query.code;
   const promise = oAuthManager.authorize(authCode);
-  console.log(authCode, promise)
+  console.log(authCode, promise);
 
-  promise.then(() => {
+  promise.then(
+    () => {
       req.session.token = lib.Configuration.oAuthToken;
-      res.redirect('/');
-  }, (exception) => {
-    console.error(exception)
-      res.redirect('/error')
+      res.redirect("/");
+    },
+    (exception) => {
+      console.error(exception);
+      res.redirect("/error");
       // error occurred, exception will be of type lib/Exceptions/OAuthProviderException
-  });
+    }
+  );
 });
-app.get("/error", function  (req, res) {
+app.get("/error", function (req, res) {
   try {
-    res.send('<a href="/">Try again</a>');  
+    res.send('<a href="/">Try again</a>');
   } catch (error) {
     res.json(error);
   }
 });
 // app.get("/", async function  (req, res) {
-app.get("/", async function  (req, res) {
-  try {
-    const user = await lib.UsersController.getCurrentUserData();
-    console.log({data: user.data})
-    res.json({data: user.data});
-    // res.send('<a href="/connect">Connect to Xero</a>');  
-  } catch (error) {
-    res.json(error);
-  }
+app.get("/", async function (req, res) {
+  const response = {
+    data: {
+      id: 1,
+      name: "John",
+      email: "john.doe@pipedrive.com",
+      date: "2019-10-01T19:20:11+02:00",
+      company_name: "Certa Pty Ltd",
+      company_domain: "certa"
+    }
+  };
+  res.json(response);
+  // try {
+  //   const user = await lib.UsersController.getCurrentUserData();
+  //   console.log({data: user.data})
+  //   res.json({data: user.data});
+  //   // res.send('<a href="/connect">Connect to Xero</a>');
+  // } catch (error) {
+  //   res.json(error);
+  // }
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
-  console.log("Your PiepDrive EndPoint basic public app is running at :" + PORT);
+  console.log(
+    "Your PiepDrive EndPoint basic public app is running at :" + PORT
+  );
 });
