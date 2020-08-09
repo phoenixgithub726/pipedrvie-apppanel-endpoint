29c433d9b77d87a2fc19840a6a96a95ce31b5f8d
Dailer Hurtado <dailer.hurtado0321@gmail.com>
2020-08-09 02:02:47 -0500

    get simple data from pipedrive /

--- app.js
+++ app.js
@@ -29,24 +29,24 @@ lib.Configuration.apiToken = 'cc638af3ea2783059aae7e32b5b80e34c1f0d1f4';
 const oAuthManager = lib.OAuthManager;
 lib.Configuration.oAuthClientId = 'de96c7b1775cbd03'; // OAuth 2 Client ID
 lib.Configuration.oAuthClientSecret = '96e47bc6458c799b36a4115aafdeabdb65075c81'; // OAuth 2 Client Secret
-lib.Configuration.oAuthRedirectUri = 'http://104.248.2.159:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
-// lib.Configuration.oAuthRedirectUri = 'http://localhost:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
+// lib.Configuration.oAuthRedirectUri = 'http://104.248.2.159:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
+lib.Configuration.oAuthRedirectUri = 'http://localhost:5000/callback'; // OAuth 2 Redirection endpoint or Callback Uri
 
 
-app.get('/', async (req, res) => {
-  if (req.session.token !== null && req.session.token !== undefined) {
-      // token is already set in the session
-      // now make API calls as required
-      // client will automatically refresh the token when it expires and call the token update callback
-      const user = await lib.UsersController.getCurrentUserData();
-      console.log("user data", user)
-      res.json(user);
-  } else {
-      const authUrl = oAuthManager.buildAuthorizationUrl();
-      console.log(authUrl)
-      res.redirect(authUrl);
-  }
-});
+// app.get('/', async (req, res) => {
+//   if (req.session.token !== null && req.session.token !== undefined) {
+//       // token is already set in the session
+//       // now make API calls as required
+//       // client will automatically refresh the token when it expires and call the token update callback
+//       const user = await lib.UsersController.getCurrentUserData();
+//       console.log("user data", user)
+//       res.json(user);
+//   } else {
+//       const authUrl = oAuthManager.buildAuthorizationUrl();
+//       console.log(authUrl)
+//       res.redirect(authUrl);
+//   }
+// });
 
 app.get('/callback', (req, res) => {
   const authCode = req.query.code;
@@ -69,15 +69,15 @@ app.get("/error", function  (req, res) {
     res.json(error);
   }
 });
-// app.get("/", async function  (req, res) {
-//   try {
-//     const user = await lib.UsersController.getCurrentUserData();
-//     res.json(user);
-//     // res.send('<a href="/connect">Connect to Xero</a>');  
-//   } catch (error) {
-//     res.json(error);
-//   }
-// });
+app.get("/", async function  (req, res) {
+  try {
+    const user = await lib.UsersController.getCurrentUserData();
+    res.json(user);
+    // res.send('<a href="/connect">Connect to Xero</a>');  
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
